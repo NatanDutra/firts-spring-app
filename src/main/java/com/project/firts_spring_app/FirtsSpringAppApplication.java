5f5ca92148865a5fd0d5656f42fd6c7433e9b928
natan-dutra-fairycode <natan.dutra@soufairycode.com>
2025-06-14 11:56:27 -0300

    init commit

---
+++ src/main/java/com/project/firts_spring_app/FirtsSpringAppApplication.java
@@ -0,0 +1,14 @@
+package com.project.firts_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+public class FirtsSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirtsSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/firts_spring_app/FirtsSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.firts_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirtsSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
